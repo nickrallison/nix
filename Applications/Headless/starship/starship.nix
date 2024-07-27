b03e19875c586917e8309f133fed9abdff06f36c
nickrallison <nickrallison1@gmail.com>
2024-07-27 14:45:51 -0600

    Sat 27 Jul 2024 14:45:51 MDT

--- Applications/Headless/starship/starship.nix
+++ Applications/Headless/starship/starship.nix
@@ -1,8 +1,8 @@
 {
-  # home.file = {
-    # "starship.toml".source = "./starship.toml";
-  # };
-
+  home.file = {
+    "starship.toml".source = ../../Headless/starship/starship.toml;
+  };
+  
   programs.starship = {
     enable = true;
   };
