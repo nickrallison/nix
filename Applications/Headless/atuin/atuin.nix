f1122ea71ac1d9e3e2544d64c4d44d6726d88f85
nickrallison <nickrallison1@gmail.com>
2024-07-28 10:17:56 -0600

    Sun 28 Jul 2024 10:17:56 MDT

--- Applications/Headless/atuin/atuin.nix
+++ Applications/Headless/atuin/atuin.nix
@@ -6,5 +6,11 @@
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
+
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://100.98.101.100:8888";
+    };
   };
 }
