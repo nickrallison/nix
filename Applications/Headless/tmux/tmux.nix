13bbb857707526fa808a097250e1888ccadf26ef
nickrallison <nickrallison1@gmail.com>
2024-07-27 22:25:11 -0600

    Sat 27 Jul 2024 22:25:11 MDT

--- Applications/Headless/tmux/tmux.nix
+++ Applications/Headless/tmux/tmux.nix
@@ -1,9 +1,15 @@
 {
-  home.file = {
-    "tmux1.conf".source = ./tmux.conf;
-  };
-  
   programs.tmux = {
     enable = true;
-  };
+    extraConfig = '' 
+      
+      unbind C-b
+      set -g prefix C-Space
+      bind C-Space send-prefix
+
+      set -sg escape-time 10
+      set -g mouse on
+      set-option -g history-limit 20000
+  '';
+  }
 }
