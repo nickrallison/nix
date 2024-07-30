3e16ddd166ae8e7fe09950bac75d969cb14a6be3
nickrallison <nickrallison1@gmail.com>
2024-07-30 10:36:51 -0600

    Tue 30 Jul 2024 10:36:51 AM MDT

--- Applications/Headless/tmux/tmux.nix
+++ Applications/Headless/tmux/tmux.nix
@@ -15,7 +15,7 @@
       set -g mouse on
       set-option -g history-limit 20000
 
-      set -g default-shell /etc/profiles/${config.home.username}/bin/zsh
+      set -g default-shell /etc/profiles/per-user/${config.home.username}/bin/zsh
 
       # DESIGN TWEAKS
 
