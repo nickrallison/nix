8efc531c2b724956a9a9007fa34042a1e3a1aa23
nickrallison <nickrallison1@gmail.com>
2024-09-22 11:27:22 -0600

    Sun 22 Sep 2024 11:27:22 MDT

--- Scripts/rsync_bisync.sh
+++ Scripts/rsync_bisync.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
-
 #### User Set Vars ####
-RSYNC_LOGS_FILE="$(date)-rsync-bisync.txt"
-RSYNC_LOGS_DIR="${LOG_DIR}/rsync_bisync_mount"
 RSYNC_MOUNT_POINT="/Users/nick/Mount/Cloud"
-# RSYNC_REMOTE="root@100.108.27.35:/mnt/user/Cloud"
 RSYNC_REMOTE="unraid:/mnt/user/Cloud"
-
-#### Formula Vars ####
-RSYNC_LOGS_PATH="${RSYNC_LOGS_DIR}/${RSYNC_LOGS_FILE}"
 
 #### Error if 
 # not connected to unraid
@@ -17,7 +10,4 @@ RSYNC_LOGS_PATH="${RSYNC_LOGS_DIR}/${RSYNC_LOGS_FILE}"
 
 
 #### Script Logic ####
-mkdir -p "${RSYNC_LOGS_DIR}"
-rclone bisync --resync ${RSYNC_MOUNT_POINT} ${RSYNC_REMOTE} >& "${RSYNC_LOGS_PATH}"
-# ssh-add --apple-load-keychain
-# rclone ls ${RSYNC_REMOTE} 
+/etc/profiles/per-user/nick/bin/rclone bisync ${RSYNC_MOUNT_POINT} ${RSYNC_REMOTE}
