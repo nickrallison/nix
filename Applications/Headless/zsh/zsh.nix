b0f54a3044ce739317869f20895490bc32c77a64
nickrallison <nickrallison1@gmail.com>
2024-09-22 19:57:38 -0600

    Sun Sep 22 19:57:38 MDT 2024

--- Applications/Headless/zsh/zsh.nix
+++ Applications/Headless/zsh/zsh.nix
@@ -63,7 +63,7 @@
         # source /etc/static/bashrc
       # fi
       export PATH="$PATH:/opt/homebrew/bin"
-      export PATH="$PATH:/etc/profiles/per-user/nick/bin"
+      # export PATH="$PATH:/etc/profiles/per-user/nick/bin"
       fastfetch
       # eval "$(zoxide init zsh)"
       # eval "$(atuin init zsh)"
