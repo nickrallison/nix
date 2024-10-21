5eb7b1700747ba30e87c34951cb1621e73c8eaba
Nick Allison <nickrallison1@gmail.com>
2024-10-21 14:09:19 -0600

    Mon 21 Oct 2024 14:09:19 MDT

--- flake.nix
+++ flake.nix
@@ -13,20 +13,20 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
 
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
-    homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
-      flake = false;
-    };
+    # homebrew-core = {
+    #   url = "github:homebrew/homebrew-core";
+    #   flake = false;
+    # };
+    # homebrew-cask = {
+    #   url = "github:homebrew/homebrew-cask";
+    #   flake = false;
+    # };
+    # homebrew-bundle = {
+    #   url = "github:homebrew/homebrew-bundle";
+    #   flake = false;
+    # };
   };
 
   outputs = inputs @ {
@@ -35,10 +35,10 @@
     nixpkgs,
     home-manager,
     nixos-wsl,
-    nix-homebrew,
-    homebrew-core,
-    homebrew-cask,
-    homebrew-bundle,
+    # nix-homebrew,
+    # homebrew-core,
+    # homebrew-cask,
+    # homebrew-bundle,
     ...
   }: {
     ##### Nix Darwin Setup ####i
@@ -59,30 +59,30 @@
           # arguments to home.nix
         }
 
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            # Install Homebrew under the default prefix
-            enable = true;
+        # nix-homebrew.darwinModules.nix-homebrew
+        # {
+        #   nix-homebrew = {
+        #     # Install Homebrew under the default prefix
+        #     enable = true;
 
-            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-            enableRosetta = true;
+        #     # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+        #     enableRosetta = true;
 
-            # User owning the Homebrew prefix
-            user = "nick";
+        #     # User owning the Homebrew prefix
+        #     user = "nick";
 
-            # Optional: Declarative tap management
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-              "homebrew/homebrew-bundle" = homebrew-bundle;
-            };
+        #     # Optional: Declarative tap management
+        #     taps = {
+        #       "homebrew/homebrew-core" = homebrew-core;
+        #       "homebrew/homebrew-cask" = homebrew-cask;
+        #       "homebrew/homebrew-bundle" = homebrew-bundle;
+        #     };
 
-            # Optional: Enable fully-declarative tap management
-            #
-            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-            # mutableTaps = false;
-          };
+        #     # Optional: Enable fully-declarative tap management
+        #     #
+        #     # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+        #     # mutableTaps = false;
+        #   };
         }
       ];
     };
