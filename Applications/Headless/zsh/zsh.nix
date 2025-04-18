69768a1a745e4d27fee730c12682c4823a7a3667
nickrallison <nickrallison1@gmail.com>
2025-04-18 16:17:29 -0600

    Fri 18 Apr 2025 16:17:29 MDT

--- Applications/Headless/zsh/zsh.nix
+++ Applications/Headless/zsh/zsh.nix
@@ -36,7 +36,7 @@
       dev = "nix develop -c zsh";
       devdir = "git submodule update --init --recursive; cd ./nix; nix develop -c zsh; cd ..";
 
-      sshubuntu = "ssh nick@100.70.42.66";
+      sshubuntu = "ssh nick@100.82.178.25";
       sshwin = "ssh desktop-oj2v1nk\\\\nick@100.110.130.8";
       sshcpsc = "ssh nicholas.allison@100.101.223.106";
       sshengg = "ssh nallison@100.88.166.10";
