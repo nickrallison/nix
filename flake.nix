39429479ac20a1505159a965d20f544a295daa38
nickrallison <nickrallison1@gmail.com>
2025-04-19 10:23:44 -0600

    Sat Apr 19 10:23:44 AM MDT 2025

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "system flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
     darwin = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -125,5 +126,20 @@
         }
       ];
     };
+
+    homeConfigurations = {
+      server = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        modules = [
+          ./Systems/VM/home.nix
+          {
+            home.username = "nick";
+            home.homeDirectory = "/home/nick";
+            programs.home-manager.enable = true;
+          }
+        ];
+      };
+    };
   };
 }
+  
