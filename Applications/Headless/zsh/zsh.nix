cadc2d00c0f97acb199de31c2565184c8c8a42c9
nickrallison <nickrallison1@gmail.com>
2025-05-03 11:33:08 -0600

    Sat May  3 11:33:08 AM MDT 2025

--- Applications/Headless/zsh/zsh.nix
+++ Applications/Headless/zsh/zsh.nix
@@ -83,11 +83,6 @@
         export PATH="$PATH:~/.local/bin"
       fi
 
-      export PYENV_ROOT="$HOME/.pyenv"
-      export PATH="$PYENV_ROOT/bin:$PATH"
-      if command -v pyenv 1>/dev/null 2>&1; then
-        eval "$(pyenv init -)"
-      fi      
     '';
   };
 }
